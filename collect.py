0ff7c38a6c9b3ce2bf255761e93457e69efa78e2
NiveskZ <krsmjogos@outlook.com>
2026-03-04 23:18:54 -0300

    feat: criando classe para exportação de dados para o S3

--- collect.py
+++ collect.py
@@ -1,10 +1,14 @@
 # %%
+import argparse
 import time
 
 import pandas as pd
 pd.set_option('display.max_columns', None)
 import fastf1
+# %%
 
+df = pd.read_parquet("data/2019_01_R.parquet")
+df
 # %%
 class CollectResults:
 
@@ -22,11 +26,20 @@ class CollectResults:
         session._load_drivers_results()
         df = session.results
 
-        df['Mode'] = mode
+        df['Year'] = session.date.year
+        df['Date'] = session.date
+        df['Mode'] = session.name
+        df['RoundNumber'] = session.event['RoundNumber']
+        df['OfficialEventName'] = session.event['OfficialEventName']
+        df['EventName'] = session.event['EventName']
+        df['Country'] = session.event['Country']
+        df['Location'] = session.event['Location']
+
         return df
 
-    def save_data(self, df, year, gp, mode):
-        df.to_parquet(f"data/{year}_{gp:02}_{mode}.parquet")
+    def save_data(self, df:pd.DataFrame, year:int, gp:int, mode:str):
+        filename = f"data/{year}_{gp:02}_{mode}.parquet"
+        df.to_parquet(filename, index=False)
 
     def process(self, year, gp, mode):
         df = self.get_data(year, gp, mode)
@@ -35,6 +48,7 @@ class CollectResults:
             return False
         
         self.save_data(df, year, gp, mode)
+        time.sleep(1)
         return True
     
     def process_year_modes(self, year):
@@ -45,10 +59,28 @@ class CollectResults:
     
     def process_years(self):
         for year in self.years:
+            print(f"Coletando dados do ano {year}")
             self.process_year_modes(year)
             time.sleep(10)
 # %%
+if __name__ == "__main__":
+    
+    parser = argparse.ArgumentParser()
 
-collect = CollectResults([2023,2024,2025], ["R", "S"])
-collect.process_years()
+    parser.add_argument("--start", type=int, default=0)
+    parser.add_argument("--stop", type=int, default=0)
+
+    parser.add_argument("--years", "-y", nargs="+", type=int)
+    parser.add_argument("--modes", "-m", nargs="+")
+
+    args = parser.parse_args()
+
+    if args.years:
+        collect = CollectResults(args.years, args.modes)
+
+    elif args.start and args.stop:
+        years = [i for i in range(args.start, args.stop + 1)]
+        collect = CollectResults(years, args.modes)
+
+    collect.process_years()
 # %%
